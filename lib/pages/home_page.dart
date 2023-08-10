6291f2c77d25a2f4b92fc5149bc788d6f2837b13
IlmanNashran <ilmannashran@gmail.com>
2023-08-10 15:34:38 +0800

    create respon variable api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:cryptocurrency_app/services/http_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,8 +12,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+  HTTPService? _http;
+
+  @override
+  void initState() {
+    super.initState();
+    _http = GetIt.instance.get<HTTPService>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              _selectedCoinDropDown(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectedCoinDropDown() {
+    List<String> _coins = ["bitcoin"];
+    List<DropdownMenuItem<String>> _items = _coins
+        .map(
+          (e) => DropdownMenuItem(
+            value: e,
+            child: Text(
+              e,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        )
+        .toList();
+    return DropdownButton(
+      value: _coins.first,
+      items: _items,
+      onChanged: (_value) {},
+      dropdownColor: const Color.fromARGB(202, 65, 30, 221),
+      icon: const Icon(
+        Icons.arrow_drop_down_sharp,
+        color: Colors.white,
+      ),
+      underline: Container(),
+    );
   }
 }
