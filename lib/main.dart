5e7617c00247742add1c1a453d24fe02312bad27
IlmanNashran <ilmannashran@gmail.com>
2023-08-10 11:53:27 +0800

    create http_service class

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:cryptocurrency_app/models/app_config.dart';
 import 'package:cryptocurrency_app/pages/home_page.dart';
+import 'package:cryptocurrency_app/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
@@ -9,6 +10,7 @@ import 'package:get_it/get_it.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await loadConfig();
+  registerHTTPService();
   runApp(const MyApp());
 }
 
@@ -22,6 +24,10 @@ Future<void> loadConfig() async {
       COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
     ),
   );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(HTTPService());
 }
 
 class MyApp extends StatelessWidget {
